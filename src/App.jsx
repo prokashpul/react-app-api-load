39264e614a6494e094f26ff3468cf8167a360202
Prokash Pul <pprokash102@gmail.com>
2022-03-16 07:10:06 +0600

    create api loder and useState distrcring and useEffect call fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
+      <h2>Welcome To Proweb react app</h2>
+      <p>Happy clint</p>
       <Countries></Countries>
     </div>
   );
@@ -12,10 +15,25 @@ function App() {
 const Countries = () => {
   return (
     <div>
-      <h2>Welcome To Proweb react app</h2>
-      <p>Happy clint</p>
+      <CountryData></CountryData>
     </div>
   );
 };
-
+// api load
+const CountryData = () => {
+  const [country, setState] = useState([]);
+  useEffect(() => {
+    const url = `https://restcountries.com/v3.1/all`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setState(data));
+  }, []);
+  console.log(country);
+  return (
+    <>
+      <h2>Welcome To Proweb react app</h2>
+      <p>Happy clint</p>
+    </>
+  );
+};
 export default App;
