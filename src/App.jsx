447abe085fa59f737ac492aa0b9e2fe1ed9c5d9d
Prokash Pul <pprokash102@gmail.com>
2022-03-16 07:02:04 +0600

    create a component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Countries></Countries>
+    </div>
+  );
+}
+// create countries component
+const Countries = () => {
+  return (
+    <div>
+      <h2>Welcome To Proweb react app</h2>
+      <p>Happy clint</p>
+    </div>
+  );
+};
+
+export default App;
